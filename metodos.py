31c972b7d06011ec2eeeaff95dca630e320fcb4b
Lucas Vinicius da Costa Santana <lvcs@ug5c30.windows.cin.ufpe.br>
2018-10-15 17:57:56 -0300

    finding euler_inverso error

--- metodos.py
+++ metodos.py
@@ -1,8 +1,8 @@
 from sympy import *
 
-y, t= symbols('y, t')
+y, t = symbols('y, t')
 
-
+arquivo_de_saida = None
 
 def Euler(y0, t0, h, qtde, func):
 	vetor_x=[]
@@ -13,44 +13,40 @@ def Euler(y0, t0, h, qtde, func):
 	t_aux=t0	
 	
 	for i in range(0, qtde):
-		y_aux= y_aux+h*func.subs([(y, y_aux), (t, t_aux)])
-		t_aux=t_aux+h
+		y_aux = y_aux + h * func.subs([(y, y_aux), (t, t_aux)])
+		t_aux = t_aux+h
 		vetor_x.append(t_aux)
 		vetor_y.append(y_aux)
-
-	ref_saida = open("saida.txt", "a")		
-	print("Metodo de Euler", file=ref_saida)
-	print("Y(" + str(t0) +")=" +str(y0), file=ref_saida)
-	print("h=" +str(h), file=ref_saida)
+	
+	print("Metodo de Euler", file=arquivo_de_saida)
+	print("y(" + str(t0) +")=" +str(y0), file=arquivo_de_saida)
+	print("h =", h, file=arquivo_de_saida)
 	
 	for j in range(0, qtde+1):
-		print(j, vetor_y[j], file=ref_saida)		
-	ref_saida.write('\n')
-	ref_saida.close()
+		print(j, vetor_y[j], file=arquivo_de_saida)		
+	arquivo_de_saida.write('\n')
 
 def Euler_inverso(y0, t0, h, qtde, func):
 	vetor_x=[]
 	vetor_y=[]
 	vetor_x.append(t0)
 	vetor_y.append(y0)
-	y_aux=y0
-	t_aux=t0
-	for i in range(1, qtde):
-		t_aux=t0+i*h
-		tn1=t_aux+h	
-		yn1=solve(y_aux+h*(func.subs(t, tn1))-y, implicit=True)
+	y_aux = y0
+	t_aux = t0
+	for i in range(0, qtde):
+		t_aux = t0 + i * h
+		tn1 = t_aux + h	
+		yn1 = solve(y_aux + h * (func.subs(t, tn1) ) - y, implicit=True)
 		vetor_y.append(yn1[0])
-		y_aux=vetor_y[i]
+		y_aux = vetor_y[i]
 
-	ref_saida = open("saida.txt", "a")		
-	print("Metodo de Euler Inverso", file=ref_saida)
-	print("Y(" + str(t0) +")=" +str(y0), file=ref_saida)
-	print("h=" +str(h), file=ref_saida)
+	print("Metodo de Euler Inverso", file=arquivo_de_saida)
+	print("y(" + str(t0) + ") = " + str(y0), file=arquivo_de_saida)
+	print("h = " + str(h), file=arquivo_de_saida)
 	
 	for j in range(0, qtde+1):
-		print(j, vetor_y[j], file=ref_saida)		
-	ref_saida.write('\n')
-	ref_saida.close()
+		print(j, vetor_y[j], file=arquivo_de_saida)		
+	arquivo_de_saida.write('\n')
 
 def Euler_aprimorado(y0, t0, h, qtde, func):
 	print("eae")
@@ -60,38 +56,38 @@ def Runge_Kutta(y0, t0, h, qtde, func):
 	vetor_y=[]
 	vetor_x.append(t0)
 	vetor_y.append(y0)
-	y_aux=y0
-	t_aux=t0
+	y_aux = y0
+	t_aux = t0
 	for i in range(0, qtde):
 		k1= func.subs([(y, y_aux), (t, t_aux)])
 		k2= func.subs([(y, y_aux+((h/2)*k1)), (t, t_aux+(h/2))])
 		k3= func.subs([(y, y_aux+((h/2)*k2)), (t, t_aux+(h/2))])
 		k4= func.subs([(y, y_aux+(h*k3)), (t, t_aux+h)])
 	
-		y_aux= y_aux + ((h/6)*(k1+(2*k2)+(2*k3)+k4))
-		t_aux= t_aux+h
+		y_aux = y_aux + ((h/6)*(k1+(2*k2)+(2*k3)+k4))
+		t_aux = t_aux + h
 		vetor_x.append(t_aux)
 		vetor_y.append(y_aux)
-
-	ref_saida = open("saida.txt", "a")		
-	print("Metodo de Runge Kutta", file=ref_saida)
-	print("Y(" + str(t0) +")=" +str(y0), file=ref_saida)
-	print("h=" +str(h), file=ref_saida)
+	
+	print("Metodo de Runge Kutta", file=arquivo_de_saida)
+	print("Y(" + str(t0) +") = " +str(y0), file=arquivo_de_saida)
+	print("h = " +str(h), file=arquivo_de_saida)
 	
 	for j in range(0, qtde+1):
-		print(j, vetor_y[j], file=ref_saida)	
+		print(j, vetor_y[j], file=arquivo_de_saida)	
 	
-	ref_saida.write('\n')
-	ref_saida.close()
+	arquivo_de_saida.write('\n')
 	
 	
 	
 def main():	
+	global arquivo_de_saida
+	arquivo_de_saida = open("saida.txt", "w")
 	ref_arquivo = open("entrada.txt", "r")
 	
 	for linha in ref_arquivo:
-		valores= linha.split()		
-		if valores[0]== 'euler':
+		valores = linha.split()		
+		if valores[0] == 'euler':
 			Euler(float(valores[1]), float(valores[2]), float(valores[3]), int(valores[4]), sympify(valores[5]))
 		elif valores[0] == 'euler_inverso':
 			Euler_inverso(float(valores[1]), float(valores[2]), float(valores[3]), int(valores[4]), sympify(valores[5]))
@@ -100,6 +96,7 @@ def main():
 		elif valores[0] == 'runge_kutta':
 			Runge_Kutta(float(valores[1]), float(valores[2]), float(valores[3]), int(valores[4]), sympify(valores[5]))
 
+	arquivo_de_saida.close()
 	
 if __name__ == '__main__': 
 	main()
